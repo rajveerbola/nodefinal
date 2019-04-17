bb77dfbcd4f5682683a22e5c68a5c464a5f5ee3e
rajveerbola <rajveer_bola@hotmail.com>
2019-04-17 10:25:41 -0700

    Hello

--- script.js
+++ script.js
@@ -12,31 +12,62 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-    respone.render(main.hbs)
+    //response.send('<h1>Hello Express!<h1>');
+    response.send({
+        name: 'Your Name',
+        school:[
+            'BCIT',
+            'SFU',
+            'UBC'
+        ]
+    })
 });
 
-app.get('/info', (request, response) => {
-   respone.render(final.hbs)
-    title:'Final Page'
+app.get('/info', (request, response)=>{
+    response.render('about.hbs', {
+        title: 'About page',
+        year: new Date().getFullYear(),
+        header: 'About Me'
+
+
+    })
 });
 
+app.get('/info', (request, response)=>{
+    response.render('about.hbs', {
+        title: 'About page',
+        year: new Date().getFullYear(),
+        header: 'About Me'
 
-app.post('/weather', (req,resp) => {
+    })
+});
 
-
-
-
-    response.render('final.hbs', {
-        final: rate
+app.get('/404', (request, response) => {
+    response.send({
+        error: 'Page not found'
     })
 
-
-
-
-
-})
-
-app.listen(port,()=>{
-    console.log(`Server is up on the port ${port}`);
 });
 
+app.get('/main',(request, response)=> {
+    response.render('main.hbs', {
+        title: 'Main Page',
+        year: new Date().getFullYear(),
+        header: 'Main Page',
+    })
+});
+
+app.get('/currency', (request, response) => {
+    response.render('currency.hbs', {
+        title: 'Currency',
+        year: new Date().getFullYear(),
+        header: 'Currency',
+    })
+});
+
+
+
+
+app.listen(port,()=>{
+    console.log('Server is up on the port 8000');
+});
