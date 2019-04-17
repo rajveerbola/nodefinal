f270bbbcd86be862d639551a2ecfff67c1a08123
rajveerbola <rajveer_bola@hotmail.com>
2019-04-16 21:16:15 -0700

    Hello

---
+++ script.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs = require('hbs')
+var app =  express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello Express!</h1>')
+});
+
+app.get('/info', (request, response) => {
+   respone.render(final.hbs)
+    title:'Final Page'
+});
+
+app.listen(port,()=>{
+    console.log(`Server is up on the port ${port}`);
+});
+
